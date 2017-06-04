b4ec2310c36dbe9fae4f7f92341d66abd0595ef9
pawelsowinski1 <pawelsowinski1@gmail.com>
2017-06-05 00:24:02 +0200

    Add files via upload

---
+++ SlashCore.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlashCore : MonoBehaviour
+{
+	GameObject obj;
+	//public GameObject parent;
+
+	Vector3 v;
+	Transform target;
+
+	void Start () 
+	{
+		Destroy(gameObject,9.5f);
+		transform.Rotate(0,0,80f);
+
+		v.Set(0,2f,0);
+
+		obj = GameObject.Find("Player");
+		target = obj.GetComponent<Transform>();
+
+		transform.position = target.position + v;
+	}
+
+	void LateUpdate()
+	{
+		transform.RotateAround(target.position, new Vector3(0,0,1), 10f);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.tag == "Enemy")
+		Destroy(gameObject,0.0f);
+	}
+}
