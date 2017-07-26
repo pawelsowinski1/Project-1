f6a4c1e40ed04bc07dba2fef465dbf99e8b4f53e
pawelsowinski1 <pawelsowinski1@gmail.com>
2017-07-26 22:13:21 +0200

    Update EnemyCore.cs

--- EnemyCore.cs
+++ EnemyCore.cs
@@ -1,8 +1,42 @@
+// 26-07-2017
+
 using UnityEngine;
 using System.Collections;
 
 public class EnemyCore : CritterCore
 {
+	GameObject player;
+	float targetX;
 
 
+
+	//==================================================
+	
+	void Start()
+	{
+		BodyInitialize();
+
+		player = GameObject.Find("Player");
+	}
+	
+	//__________________________________________________
+	
+	void Update()
+	{
+		CalculateLand();
+		PlaceOnGround();
+		DamageColorize();
+	}
+
+	void FixedUpdate()
+	{
+		targetX = player.GetComponent<Transform>().position.x;
+
+		if (targetX > transform.position.x)
+		MoveRight();
+		else
+		MoveLeft();
+	}
+	
+	//==================================================
 }
